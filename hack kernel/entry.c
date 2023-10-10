23105e23d4bf6fc028c0ff760aea9f813afab0a5
MYSK <68981944+3102802702@users.noreply.github.com>
2023-10-10 23:33:46 +0800

    Update entry.c

--- hack kernel/entry.c
+++ hack kernel/entry.c
@@ -4,98 +4,51 @@
 #include "comm.h"
 #include "memory.h"
 #include "process.h"
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/random.h>
 
-#define DEVICE_NAME "wanbai"
+#define DEVICE_NAME_LEN 20 // 设备名称的最大长度
 
-int dispatch_open(struct inode *node, struct file *file)
-{
-    return 0;
-}
+static char random_device_name[DEVICE_NAME_LEN]; // 存储随机设备节点名称的缓冲区
+static struct miscdevice my_device;
 
-int dispatch_close(struct inode *node, struct file *file)
-{
-    return 0;
-}
+static int generate_random_device_name(void) {
+    // 生成随机设备节点名称
+    int i;
+    get_random_bytes(random_device_name, DEVICE_NAME_LEN);
 
-long dispatch_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
-{
-    static COPY_MEMORY cm;
-    static MODULE_BASE mb;
-    static char key[0x100] = {0};
-    static char name[0x100] = {0};
-    static bool is_verified = false;
-
-    if (cmd == OP_INIT_KEY && !is_verified) {
-        if (copy_from_user(key, (void __user *)arg, sizeof(key) - 1) != 0) {
-            return -1;
-        }
-    }
-
-    switch (cmd) {
-        case OP_READ_MEM: {
-            if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0) {
-                return -1;
-            }
-            if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
-                return -1;
-            }
-            break;
-        }
-        case OP_WRITE_MEM: {
-            if (copy_from_user(&cm, (void __user *)arg, sizeof(cm)) != 0) {
-                return -1;
-            }
-            if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
-                return -1;
-            }
-            break;
-        }
-        case OP_MODULE_BASE: {
-            if (copy_from_user(&mb, (void __user *)arg, sizeof(mb)) != 0 ||
-                copy_from_user(name, (void __user *)mb.name, sizeof(name) - 1) != 0) {
-                return -1;
-            }
-            mb.base = get_module_base(mb.pid, name);
-            if (copy_to_user((void __user *)arg, &mb, sizeof(mb)) != 0) {
-                return -1;
-            }
-            break;
-        }
-        default:
-            break;
+    // 转换为有效的设备节点字符
+    for (i = 0; i < DEVICE_NAME_LEN; i++) {
+        random_device_name[i] = 'a' + (random_device_name[i] % 26);
     }
     return 0;
 }
 
-struct file_operations dispatch_functions = {
-    .owner = THIS_MODULE,
-    .open = dispatch_open,
-    .release = dispatch_close,
-    .unlocked_ioctl = dispatch_ioctl,
-};
+static int __init my_init(void) {
+    generate_random_device_name(); // 生成随机设备节点名称
 
-struct miscdevice misc = {
-    .minor = MISC_DYNAMIC_MINOR,
-    .name = DEVICE_NAME,
-    .fops = &dispatch_functions,
-};
+    my_device.minor = MISC_DYNAMIC_MINOR;
+    my_device.name = random_device_name;
+    my_device.fops = &dispatch_functions; // 使用您的文件操作函数
 
-int __init driver_entry(void)
-{
-    int ret;
-    printk("[+] driver_entry");
-    ret = misc_register(&misc);
-    return ret;
+    int ret = misc_register(&my_device);
+    if (ret != 0) {
+        printk(KERN_ERR "Failed to register device\n");
+        return ret;
+    }
+
+    printk(KERN_INFO "Device registered with random name: %s\n", random_device_name);
+    return 0;
 }
 
-void __exit driver_unload(void)
-{
-    printk("[+] driver_unload");
-    misc_deregister(&misc);
+static void __exit my_exit(void) {
+    misc_deregister(&my_device);
+    printk(KERN_INFO "Device unregistered\n");
 }
 
-module_init(driver_entry);
-module_exit(driver_unload);
+module_init(my_init);
+module_exit(my_exit);
 
 MODULE_DESCRIPTION("Module for kernel.");
 MODULE_LICENSE("GPL");
